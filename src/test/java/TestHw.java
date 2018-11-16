5c9e30982c5a6bc816d084fddac7cbda6bf63ed5
fedor.bazyk <Fedor.Bazyk@realdolmen.com>
2018-11-16 16:37:05 +0100

    test

--- src/test/java/TestHw.java
+++ src/test/java/TestHw.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by FBYBK60 on 11/16/2018.
@@ -11,6 +12,14 @@ public class TestHw {
     public void hwTest(){
         Main main = new Main();
         assertEquals(0, main.value);
+    -
+
+    @Test
+    public void test2(){
+
+        Main main = new Main();
+
+        assertFalse(false);
     }
 
 }
