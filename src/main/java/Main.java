40e1fc35da0cefe9fbe62bfe4584466807fb6eb7
fedor.bazyk <Fedor.Bazyk@realdolmen.com>
2018-11-16 15:16:39 +0100

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
+    public int value = 0;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
     }
 }
---
+++ src/test/java/TestHw.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by FBYBK60 on 11/16/2018.
+ */
+public class TestHw {
+
+    @Test
+    public void hwTest(){
+        Main main = new Main();
+        assertEquals(0, main.value);
+    }
+
+}
